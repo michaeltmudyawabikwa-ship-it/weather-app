b018a5016e2c746dc378e04d49d5880ada4b3b2e
Michael T Mudyawabikwa <michaeltmudyawabikwa@gmail.com>
2026-07-20 14:08:02 +0200

    fix: correct forecast url and add safety check

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const [isDark, setIsDark] = useState(false);
   const [favorites, setFavorites] = useState<string[]>([]);
 
-  const apiKey = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY
+  const API_KEY = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY
 
   useEffect(() => {
     const saved = localStorage.getItem('favorites');
@@ -33,16 +33,16 @@ export default function Home() {
     setForecast([]);
 
     try{
-      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
       if (!res.ok) throw new Error('OOPS! city not found');
       const data = await res.json();
       setWeather(data);
 
       const resForecast = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
       );
       const forecastData = await resForecast.json();
-      setForecast(forecastData.list.filter((_: any, i: number) => i % 8 === 0).slice(0,5));
+      setForecast(forecastData.list?.filter((_: any, i: number) => i % 8 === 0).slice(0,5) || []);
 
       saveFavorites(cityName);
       
